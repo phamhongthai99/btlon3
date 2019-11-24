a6c048db0c2d7faee87b47b6e46e885931d58d7e
= <=>
2019-11-25 01:02:21 +0700

    btlon3

---
+++ src/app/cauhoi/cauhoi.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cauhoi',
+  templateUrl: './cauhoi.component.html',
+  styleUrls: ['./cauhoi.component.css']
+})
+export class CauhoiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+ 
+
+}
+
